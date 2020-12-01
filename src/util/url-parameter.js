48f1708e7a9802f7ce92531c1e66e8fe21285dd5
Jonathan Gelin <jgelin@selligent.com>
2020-12-01 08:30:32 +0100

    feat: add import map override from url query param "imo"

---
+++ src/util/url-parameter.js
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+export function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
